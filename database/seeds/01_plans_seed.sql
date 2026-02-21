0ca56d18a23e2c591f098c75cd9f01686bf6f8bd
ali ki <aliki007788@gmail.com>
2026-02-21 14:05:42 +0330

    Create 01_plans_seed.sql

---
+++ database/seeds/01_plans_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO ad_plans (id, name, name_fa, price, max_ads) VALUES
+('silver', 'Silver', '???????', 250000, 10),
+('gold', 'Gold', '?????', 500000, 30),
+('diamond', 'Diamond', '?????', 1000000, 0);
+
+
