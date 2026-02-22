90be536178a477d4e6b69067621c008d413b5c82
ali ki <aliki007788@gmail.com>
2026-02-22 07:28:39 +0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-plugins: [
-vue(),
-VitePWA({
-registerType: 'autoUpdate',
-manifest: {
-name: '??????',
-short_name: '??????',
-theme_color: '#D4AF37',
-background_color: '#0A0A0A',
-display: 'standalone',
-dir: 'rtl',
-lang: 'fa'
-}
-})
-],
-server: {
-port: 3000,
-host: true
-}
+  plugins: [vue()],
+  // مشخص کردن فایل ورودی
+  build: {
+    rollupOptions: {
+      input: '/src/main.js', // فایل اصلی رو مشخص کن
+    }
+  }
 });
-
-
