87bdf4efbedc0894556df158c2c1e04e07c034a0
ali ki <aliki007788@gmail.com>
2026-02-22 07:07:11 +0330

    Create main.js

---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+// frontend/src/main.js
+import './assets/js/main.js';
+import './assets/css/main.css';
+
+// اگه از Vue استفاده می‌کنی
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
