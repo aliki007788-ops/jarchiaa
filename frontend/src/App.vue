e64c8cf02cd0611a549e9bba5c84504566bbad4f
ali ki <aliki007788@gmail.com>
2026-02-27 17:24:48 +0330

    Refactor App.vue to include header and BottomNav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div id="app">
-    <div class="mobile-view">
-      <router-view></router-view>
-    </div>
+  <div id="app" class="mobile-view">
+    <!-- منو و هدر -->
+    <header class="header">
+      <div class="menu-btn" @click="toggleDrawer">
+        <span></span><span></span><span></span>
+      </div>
+      <div class="logo" @click="switchRole">
+        <i class="fas fa-bullhorn"></i> جارچیا
+      </div>
+      <div class="header-icons">
+        <i class="fas fa-adjust" @click="toggleTheme"></i>
+        <i class="fas fa-bell" @click="showNotifications"></i>
+        <i class="fas fa-search" @click="showSearch"></i>
+      </div>
+    </header>
+
+    <!-- صفحه اصلی (از گراک) -->
+    <router-view></router-view>
+
+    <!-- ناوبری پایین -->
+    <BottomNav />
   </div>
 </template>
 
 <script>
+import BottomNav from './components/BottomNav.vue'
+import { useStore } from './store'
+
 export default {
-  name: 'App'
+  components: { BottomNav },
+  setup() {
+    const store = useStore()
+    
+    const toggleDrawer = () => store.commit('toggleDrawer')
+    const toggleTheme = () => store.commit('toggleTheme')
+    const switchRole = () => store.dispatch('switchRole')
+    const showNotifications = () => alert('۳ اعلان جدید')
+    const showSearch = () => alert('جستجو')
+
+    return { toggleDrawer, toggleTheme, switchRole, showNotifications, showSearch }
+  }
 }
 </script>
-
-<style>
-@import './assets/css/main.css';
-@import './assets/css/theme.css';
-</style>
