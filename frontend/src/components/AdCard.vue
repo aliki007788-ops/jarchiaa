413374533de24377cdd4974a55a152bb12cb5717
ali ki <aliki007788@gmail.com>
2026-02-27 17:28:19 +0330

    Add AdCard component for displaying advertisements

---
+++ frontend/src/components/AdCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ad-card" @click="$emit('click')">
+    <div class="ad-image" :style="{background: ad.color || '#D4AF37'}">
+      <div v-if="ad.badge" class="ad-badges">
+        <span class="badge" :class="ad.badge">{{ ad.badge }}</span>
+      </div>
+      <span class="ad-distance">
+        <i class="fas fa-location-dot"></i> {{ ad.distance }} متر
+      </span>
+    </div>
+    <div class="ad-content">
+      <div class="ad-header">
+        <span class="ad-title">{{ ad.title }}</span>
+        <span class="ad-price">{{ ad.price }} تومان</span>
+      </div>
+      <div class="ad-business">
+        <i class="fas fa-store"></i> {{ ad.businessName }}
+      </div>
+      <div class="ad-stats">
+        <div class="stat-item"><i class="far fa-eye"></i> {{ ad.views }}</div>
+        <div class="stat-item"><i class="far fa-heart"></i> {{ ad.likes }}</div>
+        <div class="stat-item"><i class="far fa-comment"></i> {{ ad.comments }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ad: { type: Object, required: true }
+  }
+}
+</script>
